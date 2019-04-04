3986e0ed8676c82f93adc1fad181fe980737aea0
Giovanna Rocha <giovannarrcosta@gmail.com>
2019-04-04 10:19:31 -0300

    Criação da Hooks

---
+++ src/test/java/Curso/treinamento/page/LoginPage.java
@@ -0,0 +1,26 @@
+package Curso.treinamento.page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+	public LoginPage(WebDriver driver) {
+		PageFactory.initElements(driver,  this);
+		
+	}
+	
+	@FindBy(name="userName")
+	WebElement campoUsuario;
+	
+	@FindBy(name="password")
+	WebElement campoPassword;
+	
+	@FindBy(name="login")
+	WebElement botaoSign;
+	
+	
+	
+
+}
